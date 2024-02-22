3c4e5acd31f9f8cacbdc4299e3814681079cf99f
Jimii <jimmyimpulse2@gmail.com>
2024-02-22 14:35:17 +0300

    feat: refactor

--- programs/soundwork-list/src/instructions/create_listing.rs
+++ programs/soundwork-list/src/instructions/create_listing.rs
@@ -1,13 +1,10 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount},
+    token::{self, Mint, Token, TokenAccount, TransferChecked},
 };
 
-use crate::{
-    helpers::{delegate_nft, transfer_nft},
-    state::listing::{AssetManagerV1, ListingDataV1},
-};
+use crate::state::listing::{AssetManagerV1, ListingDataV1};
 
 #[derive(Accounts)]
 pub struct CreateListing<'info> {
@@ -61,40 +58,52 @@ pub struct CreateListing<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn create_listing_handler(ctx: Context<CreateListing>, lamports: u64) -> Result<()> {
-    // insert data into listing data account
-    let listing_data_acc = &mut ctx.accounts.listing_data;
-    **listing_data_acc = ListingDataV1::new(
-        lamports,
-        ctx.accounts.authority.key(),
-        ctx.accounts.mint.key(),
-    );
+impl<'info> CreateListing<'info> {
+    pub fn validate(&self) -> Result<()> {
+        Ok(())
+    }
 
-    // signer seeds
-    let (_, bump) = Pubkey::find_program_address(&[b"soundwork".as_ref()], ctx.program_id);
-    let asset_manager_seeds = &[b"soundwork".as_ref(), &[bump]];
-    let asset_manager_signer = &[&asset_manager_seeds[..]];
+    /// creates a listing
+    #[access_control(ctx.accounts.validate())]
+    pub fn create_listing(ctx: Context<CreateListing>, lamports: u64) -> Result<()> {
+        // insert data into listing data account
+        let listing_data_acc = &mut ctx.accounts.listing_data;
+        **listing_data_acc = ListingDataV1::new(
+            lamports,
+            ctx.accounts.authority.key(),
+            ctx.accounts.mint.key(),
+        );
 
-    // delegate authority to asset manager
-    delegate_nft(
-        ctx.accounts.authority.to_account_info(),
-        ctx.accounts.asset_manager.to_account_info(),
-        ctx.accounts.authority_token_account.to_account_info(),
-        ctx.accounts.mint.clone(),
-        ctx.accounts.token_program.clone(),
-    )?;
+        // signer seeds
+        let (_, bump) = Pubkey::find_program_address(&[b"soundwork".as_ref()], ctx.program_id);
+        let asset_manager_seeds = &[b"soundwork".as_ref(), &[bump]];
+        let asset_manager_signer = &[&asset_manager_seeds[..]];
 
-    // we transfer the NFT to asset manager
-    transfer_nft(
-        ctx.accounts.authority_token_account.to_account_info(),
-        ctx.accounts.vault_token_account.to_account_info(),
-        ctx.accounts.mint.clone(),
-        ctx.accounts.authority.to_account_info(),
-        ctx.accounts.token_program.clone(),
-        asset_manager_signer,
-    )?;
+        // // delegate authority to asset manager
+        // delegate_nft(
+        //     ctx.accounts.authority.to_account_info(),
+        //     ctx.accounts.asset_manager.to_account_info(),
+        //     ctx.accounts.authority_token_account.to_account_info(),
+        //     ctx.accounts.mint.clone(),
+        //     ctx.accounts.token_program.clone(),
+        // )?;
 
-    Ok(())
+        // transfer token to asset manager vault
+        let cpi_accounts = TransferChecked {
+            from: ctx.accounts.authority_token_account.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.vault_token_account.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_context: CpiContext<'_, '_, '_, '_, TransferChecked<'_>> =
+            CpiContext::new(cpi_program, cpi_accounts);
+
+        token::transfer_checked(cpi_context, 1, 0)?;
+
+        Ok(())
+    }
 }
 
 //
